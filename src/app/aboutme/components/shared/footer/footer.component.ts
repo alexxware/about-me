80802babb7c7cad47a0c28bf0b3348beb060e29a
alexxware <manuelmorm1@gmail.com>
2025-10-16 18:34:33 -0600

    Modificacion para navegacion por url

--- src/app/aboutme/components/shared/footer/footer.component.ts
+++ src/app/aboutme/components/shared/footer/footer.component.ts
@@ -6,4 +6,10 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './footer.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FooterComponent { }
+export class FooterComponent {
+
+  goToLink(url: string)
+  {
+    window.open(url);
+  }
+}
